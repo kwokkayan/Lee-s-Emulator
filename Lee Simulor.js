c9cb9173df1cf2e0bd49a42a6331f9a05442260f
Peter-theMok <43172966+Peter-theMok@users.noreply.github.com>
2018-09-11 20:16:04 +0800

    Update Lee Simulor.js

--- Lee Simulor.js
+++ Lee Simulor.js
@@ -1,26 +1,23 @@
-  class city {
-	constructor(ID, name, investment, coor) {
-	  this.ID = ID;
-	  this.name = name;
-	  this.investment = investment;
-	  this.tier = math.round(investment / 10);
-	  this.coor = coor;
-	}
-	
-	get getName() {
-	  return this.name;
-	}
-	get getID() {
-	  return this.ID;	
-	}
-	get getCoor() {
-	  return this.coor;	
-	}
-	
-	invest(amount) {
-	  this.investment += amount;	
-	}
+class city {
+  constructor(ID, name, investment) {
+	this.ID = ID;
+	this.name = name;
+	this.investment = investment;
+	this.tier = math.round(investment / 10);
   }
+	
+  get getName() {
+	return this.name;
+  }
+  get getID() {
+	return this.ID;	
+  }
+	
+  invest(amount) {
+	this.investment += amount;	
+  }
+}
   
-  var shatin = new city;
-  document.write("Hello World");
+let shatin = new city(1, "shatin", 10);
+var nameInput = document.createElement("INPUT");
+nameInput.setAttribute("type", "text");
